689a39fd8543f8dc93a45fe17bc7549e4413b369
Avi Sahney <avisahney@Avis-MacBook-Air.local>
2023-07-24 00:43:10 +0530

    Changed name of index2.html

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require("dotenv").config();
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 
 const port = process.env.PORT || 5012;
@@ -15,5 +16,7 @@ app.use("/", require("./routes/index"));
 
 
 app.listen(port, ipadd ,() => {
+    const indexPath = path.join(__dirname, "/frontend/index.html");
     console.log(`Listening to requests on http://${ipadd}:${port}`);
+    console.log(`Open Site - File://${indexPath}`);
 });
